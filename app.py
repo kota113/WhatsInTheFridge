0009dd2470eb01346ae75f0c8e499004fc340574
Alpaca131 <66709717+Alpaca131@users.noreply.github.com>
2023-04-12 14:17:58 +0900

    add a db connection

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, session, request, render_template, url_for
+import dataset
 
 app = Flask(__name__)
+db: dataset.Database = dataset.connect("postgresql://postgres:postgres@100.65.209.33:5432/postgres")
+
+user_table: dataset.Table = db['users']
+foods_table: dataset.Table = db['foods']
 
 
 @app.route('/')
-def index():  # put application's code here
+def index():
     return render_template("index.html")
 
 @app.route('/add')
