fad5075ff64c2ff065c3b4185c232edf9fd36b59
kota113 <iwa124816+keio@gmail.com>
2023-05-04 09:29:39 +0900

    complete front end

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-import dataset
 import random
-import requests
 import string
+from datetime import datetime
+
+import dataset
+import requests
 from flask import Flask, session, request, render_template, url_for, redirect
 
 app = Flask(__name__)
@@ -14,17 +16,27 @@ foods_table: dataset.Table = db['foods']
 
 @app.route('/')
 def index():
-    # if "user_id" not in session:
-    #     return redirect(url_for('login'))
-    return render_template("index.html")
+    item_list = []
+    for row in foods_table.find():
+        n_row = dict(row)
+        expiry = datetime.strftime(row["expiry"], "%Y/%m/%d")
+        user = user_table.find_one(line_id=row["owner_line_id"])
+        n_row.update(dict(owner_name=user["line_name"], owner_room=user["room"], expiry=expiry))
+        item_list.append(n_row)
+    return render_template("index.html", item_list=item_list)
+
 
 @app.route('/add')
 def add():
+    if "line_id" not in session:
+        return redirect(url_for('login') + '?redirect_page=add')
     return render_template("add.html")
 
-@app.route('/profile')
-def profile():
-    return render_template("profile.html")
+
+@app.route('/settings')
+def settings():
+    return render_template("settings.html")
+
 
 @app.route('/api')
 def api():
@@ -33,10 +45,18 @@ def api():
 
 @app.route('/login')
 def login():
+    if "redirect_page" in request.args:
+        session['redirect_page'] = request.args['redirect_page']
     state = random_strings(15)
     session['state'] = state
     url = f"https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1660874511&redirect_uri=http%3A%2F%2F127.0.0.1%3A5000%2Fcallback&state={state}&scope=profile"
     return redirect(url)
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
 
 @app.route('/callback')
@@ -48,10 +68,13 @@ def callback():
     res = get_line_auth(code)
     session['access_token'] = res['access_token']
     res = get_current_line_user()
-    session['user_id'] = res['userId']
+    session['line_id'] = res['userId']
     session['display_name'] = res['displayName']
     session['picture_url'] = res['pictureUrl']
-    return redirect(url_for('index'))
+    redirect_page = session['redirect_page'] if "redirect_page" in session else "index"
+    user_row = user_table.find_one(line_id=session['line_id'])
+    session["room"] = user_row["room"] if user_row else None
+    return redirect(url_for(redirect_page))
 
 
 def random_strings(n):
